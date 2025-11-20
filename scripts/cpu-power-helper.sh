4e8a4418a869d6ea45c78f4520c81aeef141d4c9
Kadyn <kbelesca@aol.com>
2025-11-20 16:05:15 -0500

    - Updated helper script to call 'sudo' when calling the power-status function

--- scripts/cpu-power-helper.sh
+++ scripts/cpu-power-helper.sh
@@ -3,14 +3,14 @@
 
 case "$1" in
     "get-status")
-        governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-        cur_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
-        min_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq)
-        max_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
-        echo "$governor"
-        echo "$cur_freq"
-        echo "$min_freq"
-        echo "$max_freq"
+        sudo governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+        sudo cur_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
+        sudo min_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq)
+        sudo max_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
+        sudo echo "$governor"
+        sudo echo "$cur_freq"
+        sudo echo "$min_freq"
+        sudo echo "$max_freq"
         ;;
     "set-powersave")
         sudo echo "powersave" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
