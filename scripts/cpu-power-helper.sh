26b323774ed9abf8527b9401db3d38a7d6e0fcec
Kadyn <kbelesca@aol.com>
2025-11-20 16:24:57 -0500

    - Updated helper script to use absolute file pathing when calling 'sudo', again

--- scripts/cpu-power-helper.sh
+++ scripts/cpu-power-helper.sh
@@ -3,25 +3,25 @@
 
 case "$1" in
     "get-status")
-        governor=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-        cur_freq=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
-        min_freq=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq)
-        max_freq=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
-        sudo echo "$governor"
-        sudo echo "$cur_freq"
-        sudo echo "$min_freq"
-        sudo echo "$max_freq"
+        governor=$(/usr/bin/sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+        cur_freq=$(/usr/bin/sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
+        min_freq=$(/usr/bin/sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq)
+        max_freq=$(/usr/bin/sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
+        /usr/bin/sudo echo "$governor"
+        /usr/bin/sudo echo "$cur_freq"
+        /usr/bin/sudo echo "$min_freq"
+        /usr/bin/sudo echo "$max_freq"
         ;;
     "set-powersave")
-        sudo echo "powersave" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        sudo echo "300000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        sudo echo "600000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        /usr/bin/sudo echo "powersave" > /usr/bin/sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        /usr/bin/sudo echo "300000" > /usr/bin/sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        /usr/bin/sudo echo "600000" > /usr/bin/sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         echo "OK"
         ;;
     "set-ondemand")
-        sudo echo "ondemand" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        sudo echo "600000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        sudo echo "1500000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        /usr/bin/sudo echo "ondemand" > /usr/bin/sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        /usr/bin/sudo echo "600000" > /usr/bin/sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        /usr/bin/sudo echo "1500000" > /usr/bin/sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         echo "OK"
         ;;
     *)
