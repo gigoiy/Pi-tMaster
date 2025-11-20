e1e421327e7f87c18587efbf40606739fb02b946
Kadyn <kbelesca@aol.com>
2025-11-20 16:18:18 -0500

    - Updated helper script with proper sudo calls

--- scripts/cpu-power-helper.sh
+++ scripts/cpu-power-helper.sh
@@ -3,25 +3,25 @@
 
 case "$1" in
     "get-status")
-        sudo governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-        sudo cur_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
-        sudo min_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq)
-        sudo max_freq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
+        governor=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
+        cur_freq=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
+        min_freq=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq)
+        max_freq=$(sudo cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
         sudo echo "$governor"
         sudo echo "$cur_freq"
         sudo echo "$min_freq"
         sudo echo "$max_freq"
         ;;
     "set-powersave")
-        sudo echo "powersave" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        sudo echo "300000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        sudo echo "600000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        sudo echo "powersave" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        sudo echo "300000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        sudo echo "600000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         echo "OK"
         ;;
     "set-ondemand")
-        sudo echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        sudo echo "600000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        sudo echo "1500000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        sudo echo "ondemand" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+        sudo echo "600000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        sudo echo "1500000" > sudo /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         echo "OK"
         ;;
     *)
